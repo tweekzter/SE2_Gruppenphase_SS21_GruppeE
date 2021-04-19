bfa13684f6eaa3f92a88889c07a974de6cf58caa
AlGrade <alexandegr@edu.aau.at>
2021-04-19 11:10:46 +0200

    created settings fragment

--- app/src/main/java/com/example/se2_gruppenphase_ss21/MenuActivity.java
+++ app/src/main/java/com/example/se2_gruppenphase_ss21/MenuActivity.java
@@ -3,8 +3,12 @@ package com.example.se2_gruppenphase_ss21;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
+
+    Button settingsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +17,17 @@ public class MenuActivity extends AppCompatActivity {
         this.getSupportActionBar().hide();
 
         setContentView(R.layout.activity_menu);
+
+        settingsButton = findViewById(R.id.button_settings);
+
+        settingsButton.setOnClickListener((View v) -> {
+            openSettingsFragment();
+        });
+    }
+
+    public void openSettingsFragment(){
+        System.out.println("opening settings fragment");
+
+        getSupportFragmentManager().beginTransaction().add(R.id.container, new SettingsFragment()).commit();
     }
 }
