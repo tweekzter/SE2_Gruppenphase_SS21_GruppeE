e7e756a34832fd404803a9421aa91a4210bec57c
AlGrade <alexandegr@edu.aau.at>
2021-04-20 11:28:53 +0200

    refactored settings fragment

--- app/src/main/java/com/example/se2_gruppenphase_ss21/MainActivity.java
+++ app/src/main/java/com/example/se2_gruppenphase_ss21/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     Button gettingStartedButton;
 
