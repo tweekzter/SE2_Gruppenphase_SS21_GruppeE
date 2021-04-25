095d220ca5832c6263c3aef80ea1c4fc5f7b9a76
pilan0105 <76063789+pilan0105@users.noreply.github.com>
2021-04-25 09:48:39 +0200

    background created

---
+++ app/src/main/java/com/example/se2_gruppenphase_ss21/Dice.java
@@ -0,0 +1,13 @@
+package com.example.se2_gruppenphase_ss21;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Dice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dice);
+    }
+}
