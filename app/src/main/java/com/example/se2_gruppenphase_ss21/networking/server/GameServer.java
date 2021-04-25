3cab0b7c7aa721259d09c6971394319a7483054b
Sören Gürtler <soeren@guertler.net>
2021-04-25 23:58:55 +0200

    lower multicast timeout

--- app/src/main/java/com/example/se2_gruppenphase_ss21/networking/server/GameServer.java
+++ app/src/main/java/com/example/se2_gruppenphase_ss21/networking/server/GameServer.java
@@ -104,7 +104,7 @@ public class GameServer extends Thread {
                 }
 
                 try {
-                    sleep(10 * 1000);
+                    sleep(2 * 1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
