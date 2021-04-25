ce32c3c703c9b6a9869764dad96bb9c129c40b31
pilan0105 <76063789+pilan0105@users.noreply.github.com>
2021-04-25 10:55:50 +0200

    images added

--- app/src/main/java/com/example/se2_gruppenphase_ss21/Dice.java
+++ app/src/main/java/com/example/se2_gruppenphase_ss21/Dice.java
@@ -1,13 +1,65 @@
 package com.example.se2_gruppenphase_ss21;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Dice extends AppCompatActivity {
-
+    Button wuerfel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dice);
+        wuerfel = findViewById(R.id.roll_button);
+        wuerfel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    wuerfle();
+            }
+        });
+    }
+
+    private void wuerfle() {
+        int zahl = (int)(Math.random()*6+1);
+        TextView ergebnis = findViewById(R.id.Ergebnis);
+        ImageView bildergebnis = findViewById(R.id.diceresult);
+        switch (zahl){
+            case 1:
+                ergebnis.setText("Löwe");
+                Toast.makeText(this, "Löwe", Toast.LENGTH_SHORT).show();
+                bildergebnis.setImageResource(R.drawable.lion);
+                break;
+            case 2:
+                ergebnis.setText("Hand");
+                Toast.makeText(this, "Hand", Toast.LENGTH_SHORT).show();
+                bildergebnis.setImageResource(R.drawable.hand);
+                break;
+            case 3:
+                ergebnis.setText("Antilope");
+                Toast.makeText(this, "Antilope", Toast.LENGTH_SHORT).show();
+                bildergebnis.setImageResource(R.drawable.antilope);
+                break;
+            case 4:
+                ergebnis.setText("Schlange");
+                Toast.makeText(this, "Schlange", Toast.LENGTH_SHORT).show();
+                bildergebnis.setImageResource(R.drawable.snake);
+                break;
+            case 5:
+                ergebnis.setText("Elefant");
+                Toast.makeText(this, "Elefant", Toast.LENGTH_SHORT).show();
+                bildergebnis.setImageResource(R.drawable.elefant);
+                break;
+            case 6:
+                ergebnis.setText("Käfer");
+                Toast.makeText(this, "Käfer", Toast.LENGTH_SHORT).show();
+                bildergebnis.setImageResource(R.drawable.bug);
+                break;
+        }
     }
 }
