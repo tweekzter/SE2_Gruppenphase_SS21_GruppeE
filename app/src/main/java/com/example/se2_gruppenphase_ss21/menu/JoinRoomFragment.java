c675e6447e46f556ed9d96f22d65921f2afd9c7c
AlGrade <alexandegr@edu.aau.at>
2021-05-04 10:04:42 +0200

    created join room fragment

--- app/src/main/java/com/example/se2_gruppenphase_ss21/menu/JoinRoomFragment.java
+++ app/src/main/java/com/example/se2_gruppenphase_ss21/menu/JoinRoomFragment.java
@@ -7,16 +7,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.example.se2_gruppenphase_ss21.R;
+import com.example.se2_gruppenphase_ss21.networking.MulticastReceiver;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link LocalGameFragment#newInstance} factory method to
+ * Use the {@link JoinRoomFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class LocalGameFragment extends Fragment {
+public class JoinRoomFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,7 +29,7 @@ public class LocalGameFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public LocalGameFragment() {
+    public JoinRoomFragment() {
         // Required empty public constructor
     }
 
@@ -36,11 +38,11 @@ public class LocalGameFragment extends Fragment {
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
-     * @return A new instance of fragment LocalGameFragment.
+     * @return A new instance of fragment JoinRoomFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static LocalGameFragment newInstance(String param1) {
-        LocalGameFragment fragment = new LocalGameFragment();
+    public static JoinRoomFragment newInstance(String param1) {
+        JoinRoomFragment fragment = new JoinRoomFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         fragment.setArguments(args);
@@ -54,27 +56,14 @@ public class LocalGameFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_local_game, container, false);
-
-        String userName =  getArguments().getString(ARG_PARAM1);
-
-        Button createRoomButton = view.findViewById(R.id.button_createRoom);
-        createRoomButton.setOnClickListener((View v) -> {
-            getParentFragmentManager().beginTransaction().replace(R.id.container, CreateRoomFragment.newInstance(userName)).addToBackStack("tag").commit();
-        });
-
-        Button joinRoomButton = view.findViewById(R.id.button_joinRoom);
-        joinRoomButton.setOnClickListener((View v) -> {
-            //TODO: open joinRoomFragment with userName as param.
-            getParentFragmentManager().beginTransaction().replace(R.id.container, JoinRoomFragment.newInstance(userName)).addToBackStack("tag").commit();
-        });
-
-        return view;
+        return inflater.inflate(R.layout.fragment_join_room, container, false);
     }
 }
