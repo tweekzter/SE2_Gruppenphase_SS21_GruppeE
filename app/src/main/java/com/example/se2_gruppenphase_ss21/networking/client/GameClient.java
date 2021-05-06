f4a2519ce18207e73e7355275fc82df288e3a04e
Sören Gürtler <soeren@guertler.net>
2021-05-06 14:11:57 +0200

    client sending of message on puzzle finish

--- app/src/main/java/com/example/se2_gruppenphase_ss21/networking/client/GameClient.java
+++ app/src/main/java/com/example/se2_gruppenphase_ss21/networking/client/GameClient.java
@@ -130,6 +130,14 @@ public class GameClient {
         socket.sendString("roll " + result);
     }
 
+    public void sendPuzzleOver(int remaining, boolean didFinish) throws IOException {
+        if (!isConnected) {
+            throw new RuntimeException("Client is not connected");
+        }
+
+        socket.sendString("finish_puzzle " + (didFinish ? remaining : -1));
+    }
+
     /**
      * Sets a Listener to listen for messages from the server.
      * Only one listener can be registered at a time so this overwrites any previously registered Listener.
