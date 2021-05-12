525d59530e00a694a2b55b15062d9c423f3465dd
Manuel <msimon@gmx.at>
2021-05-12 20:45:12 +0200

    M&T: made x+y attributes protected, added hashCode()

--- app/src/main/java/com/example/se2_gruppenphase_ss21/logic/tetris/Position.java
+++ app/src/main/java/com/example/se2_gruppenphase_ss21/logic/tetris/Position.java
@@ -16,12 +16,42 @@ public class Position {
         this.y = y;
     }
 
+    /**
+     * Compares two POSITIONS with each other.
+     * @param o POSITION to compare with
+     * @return true of x and y are the same, otherwise false
+     */
     public boolean equals(Object o) {
         if(!(o instanceof Position))
             return false;
 
         Position pos = (Position) o;
         return (pos.x == this.x) && (pos.y == this.y);
+    }
+
+    /**
+     * A simple hashcode algorithm that returns the same value if the two POSITIONs equal.
+     * The values are not unique per differing POSITION, but good enough.
+     * @return a hashcode for POSITION
+     */
+    public int hashCode() {
+        return x*31 + y;
+    }
+
+    /**
+     * Returns the x-coordinate of this POSITION.
+     * @return x-coordinate
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * Returns the y-coordinate of this POSITION.
+     * @return y-coordinate
+     */
+    public int getY() {
+        return y;
     }
 }
 
