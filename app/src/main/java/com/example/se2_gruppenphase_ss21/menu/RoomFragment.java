08ebe710fda927ec6ec8253641425ece8965a930
AlGrade <alexandegr@edu.aau.at>
2021-05-15 12:21:00 +0200

    refactored RoomFragment

--- app/src/main/java/com/example/se2_gruppenphase_ss21/menu/RoomFragment.java
+++ app/src/main/java/com/example/se2_gruppenphase_ss21/menu/RoomFragment.java
@@ -1,175 +1,3 @@
-//package com.example.se2_gruppenphase_ss21.menu;
-//
-//import android.content.Context;
-//import android.os.Bundle;
-//
-//import androidx.fragment.app.Fragment;
-//
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.Button;
-//import android.widget.LinearLayout;
-//
-//import com.example.se2_gruppenphase_ss21.R;
-//import com.example.se2_gruppenphase_ss21.networking.AvailableRoom;
-//import com.example.se2_gruppenphase_ss21.networking.client.GameClient;
-//import com.example.se2_gruppenphase_ss21.networking.client.ServerMessageListener;
-//import com.example.se2_gruppenphase_ss21.networking.server.GameServer;
-//import com.example.se2_gruppenphase_ss21.networking.server.logic.GameLogicException;
-//
-//import java.io.IOException;
-//
-///**
-// * A simple {@link Fragment} subclass.
-// * Use the {@link RoomFragment#newInstance} factory method to
-// * create an instance of this fragment.
-// */
-//public class RoomFragment extends Fragment {
-//
-//    static LinearLayout layout;
-//    static Context context;
-//
-//
-//    // TODO: Rename parameter arguments, choose names that match
-//    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//    private static final String ARG_PARAM1 = "param1";
-//    private static final String ARG_PARAM2 = "param2";
-//
-//    // TODO: Rename and change types of parameters
-//    private String mParam1;
-//    private String mParam2;
-//
-//    public RoomFragment() {
-//        // Required empty public constructor
-//    }
-//
-//    /**
-//     * Use this factory method to create a new instance of
-//     * this fragment using the provided parameters.
-//     *
-//     * @param param1 Parameter 1.
-//     * @return A new instance of fragment RoomFragment.
-//     */
-//    // TODO: Rename and change types and number of parameters
-//    public static RoomFragment newInstance(String param1, AvailableRoom r) {
-//        RoomFragment fragment = new RoomFragment();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        fragment.setArguments(args);
-//
-//        String userName = param1;
-//
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                GameClient client = null;
-//                try {
-//                    client = new GameClient(r, userName);
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//
-//                try {
-//                    client.connect();
-//
-//                    ServerMessageListener listener = new ServerMessageListener() {
-//                        @Override
-//                        public void readyCount(int current, int max) {
-//                            System.out.println(current + "!!!!!!!!!!!");
-//                        }
-//
-//                        @Override
-//                        public void onGameStart() {
-//
-//                        }
-//
-//                        @Override
-//                        public void userDisconnect(String nickname) {
-//
-//                        }
-//
-//                        @Override
-//                        public void receiveUserList(String[] nicknames) {
-//                            onCreateView();
-//                            updatePlayers(nicknames);
-////                            for (String name : nicknames) {
-////                                System.out.println(name + "!!!!!!!!");
-////                            }
-//                        }
-//
-//                        @Override
-//                        public void unknownMessage(String message) {
-//
-//                        }
-//                    };
-//
-//
-//                    client.registerListener(listener);
-//                    client.startReceiveLoop();
-////                  client.sendReady(true);
-//                    // TODO: display roomname and users in this fragment... and num users and ready button; how to get users
-////                    System.out.println(listener.receiveUserList(););
-//
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                } catch (GameLogicException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }).start();
-//        return fragment;
-//    }
-//
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        context = getContext();
-//        super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
-//    }
-//
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        // Inflate the layout for this fragment
-//        View view = inflater.inflate(R.layout.fragment_room, container, false);
-//
-//        layout = view.findViewById(R.id.layoutRoom);
-//
-//        return view;
-//    }
-//
-//    public static void updatePlayers(String[] nicknames) {
-//
-////        for (String n : nicknames) {
-////            // TODO not possible from this thread.....
-////            Button newbtn;
-////            newbtn = new Button(context);
-////            newbtn.setText(n);
-////            layout.addView(newbtn);
-////            System.out.println(n);
-////
-////        }
-//
-////        Button newbtn;
-////
-////        newbtn = new Button(context);
-////        newbtn.setText(r.toString());
-////
-////        layout.addView(newbtn);
-//
-////        getParentFragmentManager().beginTransaction().replace(R.id.container, RoomFragment.newInstance(userName, r)).addToBackStack("tag").commit();
-////        do i have access to instances?
-////        getParentFragmentManager().beginTransaction().replace(R.id.container, new RoomFragment()).addToBackStack("tag").commit();
-//
-//
-//    }
-//}
-
-
 package com.example.se2_gruppenphase_ss21.menu;
 
 import android.os.Bundle;
@@ -201,9 +29,9 @@ public class RoomFragment extends Fragment {
 
 
     static AvailableRoom room;
-    String[] names;
 
-    Handler h = new Handler();
+    // Handler for creating post delay threads for updating ui
+    Handler handler = new Handler();
 
 
     // TODO: Rename parameter arguments, choose names that match
@@ -227,33 +55,28 @@ public class RoomFragment extends Fragment {
      * @return A new instance of fragment RoomFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static RoomFragment newInstance(String param1, AvailableRoom r) {
+    public static RoomFragment newInstance(String param1, AvailableRoom availableRoom) {
         RoomFragment fragment = new RoomFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
-        room = r;
+        room = availableRoom;
         fragment.setArguments(args);
-
-        String userName = param1;
-
-
+        // String userName = param1;
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
 
+        // force using ui thread for client connection
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
-        // context = getContext();
         super.onCreate(savedInstanceState);
 
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-        // System.out.println(getArguments().getString(ARG_PARAM1));
-        // updatePlayers(names, getView());
     }
 
     @Override
@@ -264,79 +87,9 @@ public class RoomFragment extends Fragment {
 
         String userName = getArguments().getString(ARG_PARAM1);
 
-        String[] test = {"a", "b", "c"};
-
-//        final Runnable r = new Runnable() {
-//            public void run() {
-//                updatePlayers(test, view);
-//            }
-//        };
-//
-//        new Handler().postDelayed(r, 1000);
-
-
-        // working
-        // updatePlayers(test, view);
-
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                GameClient client = null;
-//                try {
-//                    client = new GameClient(room, userName);
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//
-//                try {
-//                    client.connect();
-//
-//                    ServerMessageListener listener = new ServerMessageListener() {
-//                        @Override
-//                        public void readyCount(int current, int max) {
-//                            System.out.println(current + "!!!!!!!!!!!");
-//                        }
-//
-//                        @Override
-//                        public void onGameStart() {
-//
-//                        }
-//
-//                        @Override
-//                        public void userDisconnect(String nickname) {
-//
-//                        }
-//
-//                        @Override
-//                        public void receiveUserList(String[] nicknames) {
-//                            updatePlayers(nicknames, view);
-////                            for (String name : nicknames) {
-////                                System.out.println(name + "!!!!!!!!");
-////                            }
-//                        }
-//
-//                        @Override
-//                        public void unknownMessage(String message) {
-//
-//                        }
-//                    };
-//
-//
-//                    client.registerListener(listener);
-//                    client.startReceiveLoop();
-////                  client.sendReady(true);
-//                    // TODO: display roomname and users in this fragment... and num users and ready button; how to get users
-////                    System.out.println(listener.receiveUserList(););
-//
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                } catch (GameLogicException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }).start();
-
+        // create new Client
         GameClient client = null;
+
         try {
             client = new GameClient(room, userName);
         } catch (IOException e) {
@@ -346,10 +99,11 @@ public class RoomFragment extends Fragment {
         try {
             client.connect();
 
+            // implementation of ServerMessageListener Interface that is used to respond to events that are noticed by server
             ServerMessageListener listener = new ServerMessageListener() {
                 @Override
                 public void readyCount(int current, int max) {
-                    System.out.println(current + "!!!!!!!!!!!");
+
                 }
 
                 @Override
@@ -364,25 +118,7 @@ public class RoomFragment extends Fragment {
 
                 @Override
                 public void receiveUserList(String[] nicknames) {
-                    // updatePlayers(nicknames, view);
-//                            for (String name : nicknames) {
-//                                System.out.println(name + "!!!!!!!!");
-//                            }
-//                    names = nicknames;
-//                    onCreate(savedInstanceState);
                     updatePlayers(nicknames, view);
-
-
-//                    final Runnable r = new Runnable() {
-//                        public void run() {
-//                            updatePlayers(nicknames, view);
-//                            System.out.println("calling");
-//                        }
-//                    };
-//
-//                    new Handler().postDelayed(r, 1000);
-
-
                 }
 
                 @Override
@@ -391,12 +127,10 @@ public class RoomFragment extends Fragment {
                 }
             };
 
-
             client.registerListener(listener);
             client.startReceiveLoop();
-//                  client.sendReady(true);
+//          client.sendReady(true);
             // TODO: display roomname and users in this fragment... and num users and ready button; how to get users
-//                    System.out.println(listener.receiveUserList(););
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -407,13 +141,15 @@ public class RoomFragment extends Fragment {
         return view;
     }
 
+    /**
+     * updates UI: shows a list of players that currently joined the room
+     * @param nicknames all players that are in a given room
+     * @param view of the fragment
+     */
     public void updatePlayers(String[] nicknames, View view) {
 
         final Runnable r = new Runnable() {
             public void run() {
-                // updatePlayers(nicknames, view);
-                // System.out.println("calling");
-
 
                 LinearLayout layout = view.findViewById(R.id.layoutRoom);
 
@@ -429,21 +165,7 @@ public class RoomFragment extends Fragment {
             }
         };
 
-        h.postDelayed(r, 1000);
-
-
-//        LinearLayout layout = view.findViewById(R.id.layoutRoom);
-//
-//        for (String n : nicknames) {
-//            Button newbtn;
-//
-//            newbtn = new Button(getContext());
-//            newbtn.setText(n);
-//            layout.addView(newbtn);
-//            System.out.println(n);
-//
-//        }
-
+        handler.postDelayed(r, 1000);
     }
 
 
