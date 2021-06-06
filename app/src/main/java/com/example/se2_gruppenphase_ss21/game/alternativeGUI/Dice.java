86a33e28a8eba0370b73b15da9d73e3ca8045d35
Manuel <tweekzter@gmail.com>
2021-06-07 01:06:10 +0200

    getDiceCount() + animation optimizations

--- app/src/main/java/com/example/se2_gruppenphase_ss21/game/alternativeGUI/Dice.java
+++ app/src/main/java/com/example/se2_gruppenphase_ss21/game/alternativeGUI/Dice.java
@@ -7,17 +7,28 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
 import android.view.animation.LinearInterpolator;
 import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
 import android.widget.TableLayout;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.se2_gruppenphase_ss21.R;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 public class Dice extends Fragment {
+    private static final int ANTILOPE_POS = 325;
+    private static final int LION_POS = 775;
+    private static final int ELEPHANT_POS = 1225;
+    private static final int BUG_POS = 1675;
+    private static final int HAND_POS = 2125;
+    private static final int SNAKE_POS = 2575;
+
+
+
     public View onCreateView(LayoutInflater inflater, ViewGroup puzzleContainer,
                              Bundle savedInstanceState) {
 
@@ -29,23 +40,57 @@ public class Dice extends Fragment {
 
     private void animateDiceRoll() {
         TableLayout frame = getView().findViewById(R.id.frame_roll);
-        ObjectAnimator animator = ObjectAnimator.ofFloat(frame, "translationX", 7700);
+        float distance = frame.getWidth();
+
+        ObjectAnimator animator = ObjectAnimator.ofFloat(frame,
+                "translationX", distance);
         animator.setInterpolator(new LinearInterpolator());
-        animator.setDuration(4000);
-        animator.setRepeatCount(5);
-        animator.start();
+        animator.setDuration(2500);
+        ObjectAnimator reset = ObjectAnimator.ofFloat(frame,
+                "translationX", 0f);
+        reset.setInterpolator(new LinearInterpolator());
+        reset.setDuration(0);
+        float random = (float)Math.random();
+        ObjectAnimator stop = ObjectAnimator.ofFloat(frame,
+                "translationX", distance * random);
+        stop.setInterpolator(new DecelerateInterpolator());
+        stop.setDuration((int)(2500 * random * 2));
+        AnimatorSet animation = new AnimatorSet();
+        animation.playSequentially(animator, reset, stop);
+        animation.start();
 
         new Thread(() -> {
-            int time = 0;
-            while(time < 5000) {
-                Log.d("dicebar", "loc: "+frame.getTranslationX());
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                time += 500;
+            try {
+                Thread.sleep(5001);
+                Log.d("dice", "result: "+getDiceResult(convertPixelsToDp((frame.getTranslationX()))));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }).start();
+    }
+
+    private static int getDiceResult() {
+        return 1;
+    }
+
+    private float convertDpToPixels(float dp) {
+        return dp * getContext().getResources().getDisplayMetrics().density;
+    }
+
+    private float convertPixelsToDp(float pixels) {
+        return pixels / getContext().getResources().getDisplayMetrics().density;
+    }
+
+    private int getDiceResult(float result) {
+        int[] diceCounts = { ANTILOPE_POS, LION_POS, ELEPHANT_POS, BUG_POS, HAND_POS, SNAKE_POS };
+        int selected = ANTILOPE_POS;
+
+        for(int diceCount : diceCounts) {
+            if(Math.abs(result - diceCount) <= 250) {
+                selected = diceCount;
+                break;
+            }
+        }
+        return selected;
     }
 }
--- app/src/main/java/com/example/se2_gruppenphase_ss21/menu/MainActivity.java
+++ app/src/main/java/com/example/se2_gruppenphase_ss21/menu/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 
 import com.example.se2_gruppenphase_ss21.R;
 import com.example.se2_gruppenphase_ss21.game.Dice;
+import com.example.se2_gruppenphase_ss21.game.alternativeGUI.Puzzle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,6 +36,11 @@ public class MainActivity extends AppCompatActivity {
         test.setOnClickListener((View v) -> {
             openGame();
         });
+
+        Button altPuzzle = findViewById(R.id.alt_puzzle);
+        altPuzzle.setOnClickListener((View v) -> {
+            openAlternativePuzzle();
+        });
     }
 
     public void openMenuActivity() {
@@ -44,6 +50,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void openGame(){
         Intent intent = new Intent(this, Dice.class);
+        startActivity(intent);
+    }
+
+    public void openAlternativePuzzle(){
+        Intent intent = new Intent(this, Puzzle.class);
         startActivity(intent);
     }
 }
