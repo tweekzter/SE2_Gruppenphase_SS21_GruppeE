2bebe5d5d63630015165d50a36259eeaaf6b89f8
Sören Gürtler <soeren@guertler.net>
2021-06-09 13:06:30 +0200

    move listener impl to testing

--- app/src/test/java/com/example/se2_gruppenphase_ss21/ServerMessageListenerImpl.java
+++ app/src/test/java/com/example/se2_gruppenphase_ss21/ServerMessageListenerImpl.java
@@ -1,4 +1,4 @@
-package com.example.se2_gruppenphase_ss21.networking.client;
+package com.example.se2_gruppenphase_ss21;
 
 import com.example.se2_gruppenphase_ss21.networking.client.listeners.*;
 
@@ -29,12 +29,12 @@ public class ServerMessageListenerImpl implements GeneralGameListener, InRoundLi
     }
 
     @Override
-    public void rollRequest(String nick) {
+    public void playDiceAnimation(int result) {
 
     }
 
     @Override
-    public void rollResult(int result) {
+    public void transitionToPuzzle() {
 
     }
 
