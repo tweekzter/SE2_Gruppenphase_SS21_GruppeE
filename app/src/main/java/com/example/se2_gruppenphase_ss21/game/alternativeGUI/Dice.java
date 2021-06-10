2a5a237cb2bfafbfb631e6cf454363c242da0cb5
Manuel <tweekzter@gmail.com>
2021-06-10 02:12:13 +0200

    first implementation of PlayField

--- app/src/main/java/com/example/se2_gruppenphase_ss21/game/alternativeGUI/Dice.java
+++ app/src/main/java/com/example/se2_gruppenphase_ss21/game/alternativeGUI/Dice.java
@@ -3,18 +3,21 @@ package com.example.se2_gruppenphase_ss21.game.alternativeGUI;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.DecelerateInterpolator;
 import android.view.animation.LinearInterpolator;
-import android.widget.Button;
 import android.widget.TableLayout;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.se2_gruppenphase_ss21.R;
+import com.example.se2_gruppenphase_ss21.menu.StartGameFragment;
+import com.example.se2_gruppenphase_ss21.networking.Util;
 
 
 public class Dice extends Fragment {
@@ -25,6 +28,7 @@ public class Dice extends Fragment {
     private static final int BUG_POS = 1675;
     private static final int HAND_POS = 2125;
     private static final int SNAKE_POS = 2575;
+    private int diceResult = 325;
     private int animationTime = 5000;
 
 
@@ -33,9 +37,13 @@ public class Dice extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_dice, puzzleContainer, false);
-        Button b = view.findViewById(R.id.roll_dice);
-        b.setOnClickListener(v -> animateDiceRoll());
         return view;
+    }
+
+    public void onStart() {
+        super.onStart();
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(() -> animateDiceRoll(), 500);
     }
 
     private void animateDiceRoll() {
@@ -50,7 +58,8 @@ public class Dice extends Fragment {
         reset.setInterpolator(new LinearInterpolator());
         reset.setDuration(0);
 
-        float target = convertDpToPixels(getDiceResult());
+        diceResult = getDiceResult();
+        float target = convertDpToPixels(diceResult);
         ObjectAnimator stop = ObjectAnimator.ofFloat(frame, "translationX", target);
         stop.setInterpolator(new DecelerateInterpolator());
         stop.setDuration(animationTime/2);
@@ -60,16 +69,13 @@ public class Dice extends Fragment {
         animation.playSequentially(rollAnimation, reset, stop);
         animation.start();
 
-
-        new Thread(() -> {
-            try {
-                Thread.sleep(animationTime+1000);
-                Log.d("dice", "pos: "+convertPixelsToDp(frame.getTranslationX()));
-
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }).start();
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(() -> {
+            getParentFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.puzzle_container, new PlayField())
+                    .commit();
+            }, animationTime + 1000);
     }
 
 
@@ -85,13 +91,5 @@ public class Dice extends Fragment {
     private int getDiceResult() {
         int[] diceCounts = { ANTILOPE_POS, LION_POS, ELEPHANT_POS, BUG_POS, HAND_POS, SNAKE_POS };
         return diceCounts[(int)(diceCounts.length * Math.random())];
-    }
-
-    public int getAnimationTime() {
-        return animationTime;
-    }
-
-    public void setAnimationTime(int time) {
-        animationTime = time;
     }
 }
---
+++ app/src/main/java/com/example/se2_gruppenphase_ss21/game/alternativeGUI/PlayField.java
@@ -0,0 +1,97 @@
+package com.example.se2_gruppenphase_ss21.game.alternativeGUI;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.se2_gruppenphase_ss21.R;
+
+public class PlayField extends Fragment {
+    @SuppressLint("ClickableViewAccessibility")
+    public View onCreateView(LayoutInflater inflater, ViewGroup puzzleContainer,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_playfield, puzzleContainer, false);
+        TextView text = view.findViewById(R.id.testi);
+        view.findViewById(R.id.touch_field).setOnTouchListener((v, event) -> {
+            int x = (int)convertPixelsToDp(event.getX()) / 76;
+            int y = (int)convertPixelsToDp(event.getY()) / 76;
+            getField(x,y).setBackgroundColor(Color.RED);
+            return true;
+        });
+        view.findViewById(R.id.field20).setOnClickListener(v -> text.setText("cougar"));
+        return view;
+    }
+
+    private float convertDpToPixels(float dp) {
+        return dp * getContext().getResources().getDisplayMetrics().density;
+    }
+
+    private float convertPixelsToDp(float pixels) {
+        return pixels / getContext().getResources().getDisplayMetrics().density;
+    }
+
+    private View getField(int x, int y) {
+        String search = x+","+y;
+        switch(search) {
+            case("0,0"):
+                return getView().findViewById(R.id.field00);
+            case("1,0"):
+                return getView().findViewById(R.id.field10);
+            case("2,0"):
+                return getView().findViewById(R.id.field20);
+            case("3,0"):
+                return getView().findViewById(R.id.field30);
+            case("4,0"):
+                return getView().findViewById(R.id.field40);
+            case("0,1"):
+                return getView().findViewById(R.id.field01);
+            case("1,1"):
+                return getView().findViewById(R.id.field11);
+            case("2,1"):
+                return getView().findViewById(R.id.field21);
+            case("3,1"):
+                return getView().findViewById(R.id.field31);
+            case("4,1"):
+                return getView().findViewById(R.id.field41);
+            case("0,2"):
+                return getView().findViewById(R.id.field02);
+            case("1,2"):
+                return getView().findViewById(R.id.field12);
+            case("2,2"):
+                return getView().findViewById(R.id.field22);
+            case("3,2"):
+                return getView().findViewById(R.id.field32);
+            case("4,2"):
+                return getView().findViewById(R.id.field42);
+            case("0,3"):
+                return getView().findViewById(R.id.field03);
+            case("1,3"):
+                return getView().findViewById(R.id.field13);
+            case("2,3"):
+                return getView().findViewById(R.id.field23);
+            case("3,3"):
+                return getView().findViewById(R.id.field33);
+            case("4,3"):
+                return getView().findViewById(R.id.field43);
+            case("0,4"):
+                return getView().findViewById(R.id.field04);
+            case("1,4"):
+                return getView().findViewById(R.id.field14);
+            case("2,4"):
+                return getView().findViewById(R.id.field24);
+            case("3,4"):
+                return getView().findViewById(R.id.field34);
+            case("4,4"):
+                return getView().findViewById(R.id.field44);
+            default:
+                return getView().findViewById(R.id.field44);
+        }
+    }
+}
