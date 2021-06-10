bd7064530b22c7ee2dd11bec6b5e5c7c02d9354a
Sören Gürtler <soeren@guertler.net>
2021-06-10 19:32:16 +0200

    adjust round count and time

--- app/src/main/java/com/example/se2_gruppenphase_ss21/networking/server/logic/GameRoom.java
+++ app/src/main/java/com/example/se2_gruppenphase_ss21/networking/server/logic/GameRoom.java
@@ -9,8 +9,8 @@ import java.util.*;
 
 public class GameRoom {
     private static final int DEFAULT_MAX_USERS = 4;
-    private static final int ROUND_COUNT = 9;
-    private static final int PUZZLE_DURATION = 60 * 1000;
+    private static final int ROUND_COUNT = 5;
+    private static final int PUZZLE_DURATION = 2 * 60 * 1000;
 
     private ArrayList<GameClientHandler> handlers = new ArrayList<>();
 
