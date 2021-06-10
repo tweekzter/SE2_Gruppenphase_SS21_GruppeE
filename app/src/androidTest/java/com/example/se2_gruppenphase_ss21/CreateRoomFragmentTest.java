5b1328733b2b98c1696d639da20091755084aeac
Manfred Madritsch <made@gmx.at>
2021-06-10 16:13:48 +0200

    CreateRoomFragmentTest

---
+++ app/src/androidTest/java/com/example/se2_gruppenphase_ss21/CreateRoomFragmentTest.java
@@ -0,0 +1,47 @@
+package com.example.se2_gruppenphase_ss21;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.testing.FragmentScenario;
+import androidx.lifecycle.Lifecycle;
+import androidx.test.core.app.ApplicationProvider;
+
+import com.example.se2_gruppenphase_ss21.menu.CreateRoomFragment;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertTrue;
+
+public class CreateRoomFragmentTest {
+
+    @Test
+    public void testCreateRoom() {
+        FragmentScenario scenario = FragmentScenario.launchInContainer(CreateRoomFragment.class, Bundle.EMPTY);
+
+        onView(withId(R.id.editTextNameOfRoom))
+                .perform(typeText("testa"), closeSoftKeyboard());
+
+        onView(withId(R.id.editTextMaxUsers))
+                .perform(typeText("zwei"), closeSoftKeyboard());
+
+        onView(withId(R.id.editTextMaxUsers))
+                .check(matches(withText("")));
+
+        onView(withId(R.id.editTextMaxUsers))
+                .perform(typeText("4"), closeSoftKeyboard());
+
+        onView(withId(R.id.editTextMaxUsers))
+                .check(matches(withText("4")));
+
+    }
+
+}
