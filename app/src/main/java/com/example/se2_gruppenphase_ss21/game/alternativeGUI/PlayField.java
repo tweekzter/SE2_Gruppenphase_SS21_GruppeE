1bb8e69010a04b9e3ce926cdf9d7acb05f5328d7
Manuel <tweekzter@gmail.com>
2021-06-15 14:45:25 +0200

    remove dunctionality added - tray layout reduced to 4x4

--- app/src/main/java/com/example/se2_gruppenphase_ss21/game/alternativeGUI/PlayField.java
+++ app/src/main/java/com/example/se2_gruppenphase_ss21/game/alternativeGUI/PlayField.java
@@ -2,6 +2,7 @@ package com.example.se2_gruppenphase_ss21.game.alternativeGUI;
 
 import android.annotation.SuppressLint;
 import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,6 +12,7 @@ import android.view.ViewGroup;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
+import androidx.core.graphics.ColorUtils;
 import androidx.fragment.app.Fragment;
 
 import com.example.se2_gruppenphase_ss21.R;
@@ -36,6 +38,10 @@ public class PlayField extends Fragment implements InRoundListener, View.OnTouch
     private Map map;
     private int cursorOffsetX = 0;
     private int cursorOffsetY = 0;
+
+    private static final int TILE1_COLOR = Color.RED;
+    private static final int TILE2_COLOR = Color.BLUE;
+    private static final int TILE3_COLOR = Color.YELLOW;
 
     private TableLayout mapTable;
     private TableLayout trayTile1;
@@ -77,45 +83,81 @@ public class PlayField extends Fragment implements InRoundListener, View.OnTouch
 
         setUpTiles();
 
-        setUpTileTray();
+        assignTrayTiles();
+        addTileToTray(tile1, trayTile1);
+        addTileToTray(tile2, trayTile2);
+        addTileToTray(tile3, trayTile3);
         registerTrayTileListener(trayTile1, tile1);
         registerTrayTileListener(trayTile2, tile2);
         registerTrayTileListener(trayTile3, tile3);
 
+        setUpButtons();
+
+    }
+
+    private void setUpButtons() {
         getView().findViewById(R.id.rotate_right).setOnClickListener(v -> {
             if(active == null) return;
+            if(active.detachFromMap())
+                setOriginalColor(active);
             active.rotateRightAndPlace(map);
+            attachTile(active);
             drawMap();
         });
 
         getView().findViewById(R.id.rotate_left).setOnClickListener(v -> {
             if(active == null) return;
+            if(active.detachFromMap())
+                setOriginalColor(active);
             active.rotateLeftAndPlace(map);
+            attachTile(active);
             drawMap();
         });
 
         getView().findViewById(R.id.mirror_horizontal).setOnClickListener(v -> {
             if(active == null) return;
+            if(active.detachFromMap())
+                setOriginalColor(active);
             active.mirrorHorizontallyAndPlace(map);
+            attachTile(active);
             drawMap();
         });
 
         getView().findViewById(R.id.mirror_vertical).setOnClickListener(v -> {
             if(active == null) return;
+            if(active.detachFromMap())
+                setOriginalColor(active);
             active.mirrorVerticallyAndPlace(map);
+            attachTile(active);
             drawMap();
+        });
+
+        getView().findViewById(R.id.remove).setOnClickListener(v -> {
+            if(active == null) return;
+
+            if(active == tile1)
+                addTileToTray(active, trayTile1);
+            else if(active == tile2)
+                addTileToTray(active, trayTile2);
+            else
+                addTileToTray(active, trayTile3);
+
+            detachTile(active);
+            active.removeTempFromMap();
+            drawMap();
+            active = null;
         });
     }
 
     private void setUpTiles() {
         tile1 = new Tile(getContext().getAssets(), tileIDs[0], "standard");
-        tile1.setColor(Color.RED);
+        setOriginalColor(tile1);
 
         tile2 = new Tile(getContext().getAssets(), tileIDs[1], "standard");
-        tile2.setColor(Color.BLUE);
+        setOriginalColor(tile2);
 
         tile3 = new Tile(getContext().getAssets(), tileIDs[2], "standard");
-        tile3.setColor(Color.YELLOW);
+        setOriginalColor(tile3);
     }
 
     private void drawMap() {
@@ -140,30 +182,42 @@ public class PlayField extends Fragment implements InRoundListener, View.OnTouch
         }
     }
 
-    private void setUpTileTray() {
-
-        boolean[][] matrix = tile1.getShapeMatrix();
+    private void assignTrayTiles() {
         trayTile1 = getView().findViewById(R.id.table_tile1);
-        drawTrayTile(trayTile1, matrix, Color.RED);
-
-        matrix = tile2.getShapeMatrix();
         trayTile2 = getView().findViewById(R.id.table_tile2);
-        drawTrayTile(trayTile2, matrix, Color.BLUE);
-
-        matrix = tile3.getShapeMatrix();
         trayTile3 = getView().findViewById(R.id.table_tile3);
-        drawTrayTile(trayTile3, matrix, Color.YELLOW);
+    }
+
+    private void addTileToTray(Tile tile, TableLayout trayTile) {
+
+        boolean[][] matrix = tile.getShapeMatrix();
+        setOriginalColor(tile);
+        drawTrayTile(trayTile, matrix, tile.getColor());
+        trayTile.setClickable(true);
+        trayTile.setVisibility(View.VISIBLE);
     }
 
     private void drawTrayTile(TableLayout table, boolean[][] matrix, int color) {
         // offset to place it more centric
         int offsetX = (4 - matrix[0].length) / 2;
-        int offsetY = (5 - matrix.length) / 2;
+        int offsetY = (4 - matrix.length) / 2;
 
+        clearTrayTileMap(table);
         for(int y=0; y < matrix.length; y++)
             for(int x=0; x < matrix[0].length; x++)
-                if(matrix[y][x])
-                    getBox(table, x + offsetX, y + offsetY).setBackgroundColor(color);
+                if(matrix[y][x] && (x+offsetX) < 4 && (x+offsetX) >= 0
+                        && (y+offsetY) < 4 && (y+offsetY) >=0) {
+
+                    View box = getBox(table, x + offsetX, y + offsetY);
+                    box.setAlpha(1);
+                    box.setBackgroundColor(color);
+                }
+    }
+
+    private void clearTrayTileMap(TableLayout table) {
+        for(int y=0; y < 4; y++)
+            for(int x=0; x < 4; x++)
+                getBox(table, x, y).setAlpha(0);
     }
 
     @Override
@@ -184,18 +238,51 @@ public class PlayField extends Fragment implements InRoundListener, View.OnTouch
         if(event.getAction() == MotionEvent.ACTION_DOWN) {
             if(box.isCoveredByTempTile()) {
                 active = onLocation;
+                detachTile(active);
                 cursorOffsetX = active.getHook().getX() - x;
                 cursorOffsetY = active.getHook().getY() - y;
             }
         }
-        else {
-            if(active.getHook().getX() != x || active.getHook().getY() != y) {
+        else if(event.getAction() == MotionEvent.ACTION_UP) {
+            if(active != null && attachTile(active))
+                drawMap();
+        }
+        else if(event.getAction() == MotionEvent.ACTION_MOVE) {
+            if(active != null && (active.getHook().getX() != x || active.getHook().getY() != y)) {
                 active.placeTempOnMap(map, new Position(x + cursorOffsetX,y + cursorOffsetY));
                 drawMap();
             }
         }
 
         return true;
+    }
+
+    private boolean attachTile(Tile tile) {
+        boolean attached = tile.attachToMap(map, tile.getHook());
+        if(attached) {
+            int attachedColor = ColorUtils.blendARGB(
+                    tile.getColor(), Color.BLACK, 0.3f);
+            tile.setColor(attachedColor);
+            MediaPlayer mp = MediaPlayer.create(getContext(), R.raw.click);
+            mp.start();
+        }
+        return attached;
+    }
+
+    private boolean detachTile(Tile tile) {
+        boolean detached = tile.detachFromMap();
+        if(detached)
+            setOriginalColor(tile);
+        return detached;
+    }
+
+    private void setOriginalColor(Tile tile) {
+        if(tile == tile1)
+            tile.setColor(TILE1_COLOR);
+        else if(tile == tile2)
+            tile.setColor(TILE2_COLOR);
+        else
+            tile.setColor(TILE3_COLOR);
     }
 
     private View getBox(TableLayout table, int x, int y) {
