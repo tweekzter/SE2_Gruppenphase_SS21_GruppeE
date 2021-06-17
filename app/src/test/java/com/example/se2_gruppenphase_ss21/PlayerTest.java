00906ef9a7f0c9a997821ad725ac5fe362d3a158
SabrinaMuh <sabrina.muhrer@aon.at>
2021-06-17 20:41:02 +0200

    add tests

--- app/src/test/java/com/example/se2_gruppenphase_ss21/PlayerTest.java
+++ app/src/test/java/com/example/se2_gruppenphase_ss21/PlayerTest.java
@@ -7,9 +7,26 @@ public class PlayerTest {
 
     @Test
     public void testPlayer1(){
-        Player player = new Player("1", "Bob", "2 points");
+        Player player = new Player("1", "Player1", "2 points");
         assertEquals("1", player.getPosition());
-        assertEquals("Bob", player.getName());
+        assertEquals("Player1", player.getName());
         assertEquals("2 points", player.getPoints());
+    }
+
+    @Test
+    public void testPlayer2(){
+        Player player = new Player();
+        player.setPosition("2");
+        player.setName("Player2");
+        player.setPoints("1 points");
+        assertEquals("2", player.position);
+        assertEquals("Player1", player.getName());
+        assertEquals("1 points", player.getPoints());
+    }
+
+    @Test
+    public void testPlayer3(){
+        Player player = new Player("3", "Player3", "1 points");
+        assertEquals("Player{position=3, name='Player3', points=1 points}", player.toString());
     }
 }
